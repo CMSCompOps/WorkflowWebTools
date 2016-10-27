45da55d60d0bacc8ce44927463f588c006d4eeea
Daniel Robert Abercrombie <dabercro@mit.edu>
2016-10-28 01:13:57 +0200

    Some errors are happening. Trying to handle.

--- globalerrors.py
+++ globalerrors.py
@@ -66,12 +66,26 @@ class ErrorInfo(object):
             curs.execute('SELECT DISTINCT {0} FROM workflows'.format(column))
             return [entry[0] for entry in curs.fetchall()]
 
+        def safe_int(element):
+            """A sorting algorithm that strings don't break.
+
+            :params str element: A string that should be a number,
+                                 but is taken care of in the event that it's not.
+            :returns: Either the string as an integer or the string unchanged.
+            :rtype: int or str
+            """
+            try:
+                return int(element)
+            except ValueError:
+                return element
+
+
         allsteps = get_all('stepname')
         allsteps.sort()
         allsites = get_all('sitename')
         allsites.sort()
         allerrors = get_all('errorcode')
-        allerrors.sort(key=int)
+        allerrors.sort(key=safe_int)
 
         data_location = serverconfig.explain_errors_path()
 
