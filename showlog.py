b6f8b27d053431b9c17e8b2299009f929ad7f5fd
dabercro <dabercro@mit.edu>
2016-12-02 13:25:17 +0100

    Added functionality to showlog.

--- showlog.py
+++ showlog.py
@@ -8,18 +8,22 @@ Generates content for the showlog webpage
 from CMSToolBox.elasticsearch import search_logs
 
 
-def give_logs(query):
+def give_logs(query, module='', limit=50):
     """Takes output from :py:func:`CMSToolBox.elasticsearch.search_logs`
     and organizes it to pass to a mako template
 
     :param str query: The query to pass to :py:func:`CMSToolBox.elasticsearch.search_logs`
+    :param str module: The module to show
+    :param int limit: The max number of logs to show on a single page
     :returns: dictionary with ['logs'], which is a list of logs, and ['meta'],
               which shows meta information of newest query result
     :rtype: dict
     """
 
     formtext = ('<form>Submit query: <input type="text" name="search"> '
-                '<input type="submit" value="Submit"></form>')
+                'Module: <input type="text" name="module" value="%s"> '
+                'Logs Limit: <input type="text" name="limit" value = "%i"> '
+                '<input type="submit" value="Submit"></form>' % (module, limit))
 
     if query == '':
         # Get form page
@@ -34,20 +38,22 @@ def give_logs(query):
         logs = list()
 
         for i in output:
-            if len(texts) > 50:
+            if len(texts) > limit:
                 break
             if i['_source']['text'] in texts:
                 continue
 
-            logs.append(
-                {
-                    'subject': i['_source']['subject'],
-                    'date':    i['_source']['date'],
-                    'text':    i['_source']['text'].split('\n')
-                }
-            )
+            if not module or  i['_source']['subject'] == module:
 
-            texts.add(i['_source']['text'])
+                logs.append(
+                    {
+                        'subject': i['_source']['subject'],
+                        'date':    i['_source']['date'],
+                        'text':    i['_source']['text'].split('\n')
+                    }
+                )
+
+                texts.add(i['_source']['text'])
 
         return {
             'logs': logs,
