edb5c4ab60ed87c21c19c25c7d623404c3fde77c
Weinan Si <siweinan@gmail.com>
2018-08-24 16:38:19 +0200

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,6 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0, os.path.abspath('../'))
-#print sys.path
-#sys.path.insert(0, os.path.abspath('../runserver'))
 
 
 # -- Project information -----------------------------------------------------
