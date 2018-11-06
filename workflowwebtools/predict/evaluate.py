3d232915b0765f4bcd7df9696e3515b62931b6b4
Daniel Abercrombie <dabercro@mit.edu>
2018-11-06 16:41:29 -0500

    Add some docstrings

--- workflowwebtools/predict/evaluate.py
+++ workflowwebtools/predict/evaluate.py
@@ -1,4 +1,14 @@
+"""
+A module that evaluates a model and returns the prediction
+"""
 
-def predict(errors):
+def predict(errors): # pylint: disable=unused-argument
+    """
+    Takes the errors for a workflow and makes an action prediction
+    :param errors: Errors for a workflow in whatever format we like
+    :type errors: Don't know yet
+    :returns: Prediction results
+    :rtype: dict
+    """
 
     return {'Action': 'TBD'}
